c242ccf4bb6df4391133a35ddbc1a8b911a5a15e
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-05 15:37:28 -0400

    finished the method that splits string at the empty space and returns an array, for which i check the length of the array

---
+++ methods.rb
@@ -0,0 +1,12 @@
+def word_counter(string)
+  array = string.split(" ")
+  puts array.length
+end
+
+
+word_counter("this is a test of my method")
+word_counter("Hello world") # returns 2
+
+word_counter("This is a sentence") # returns 4
+
+word_counter("") # returns 0
